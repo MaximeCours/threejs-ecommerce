93c28b702a619bbe5229dabf52664e4ee8fb3644
Karen160 <karen.azoulay@edu.devinci.fr>
2023-06-20 14:25:17 +0200

    indentation

--- src/page/pins/pins.js
+++ src/page/pins/pins.js
@@ -1,130 +1,133 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 const allPins = [
-    {
-        querySelector: '.pig',
-        form: 'square',
-        color: 0xde9ba3,
-        texture: 'pig.png',
-    },
-    {
-        querySelector: '.human',
-        form: 'square',
-        color: 0xCD9971,
-        texture: 'human.png',
-    },
-    {
-        querySelector: '.monster',
-        form: 'square',
-        color: 0x2D2A2D,
-        texture: 'monster.png',
-    },
-    {
-        querySelector: '.person',
-        form: 'square',
-        color: 0xECCDA8,
-        texture: 'person.png',
-    },
-    {
-        querySelector: '.screamer',
-        form: 'square',
-        color: 0x4CA15F,
-        texture: 'screamer.png',
-    }
+  {
+    querySelector: '.pig',
+    form: 'square',
+    color: 0xde9ba3,
+    texture: 'pig.png',
+  },
+  {
+    querySelector: '.human',
+    form: 'square',
+    color: 0xcd9971,
+    texture: 'human.png',
+  },
+  {
+    querySelector: '.monster',
+    form: 'square',
+    color: 0x2d2a2d,
+    texture: 'monster.png',
+  },
+  {
+    querySelector: '.person',
+    form: 'square',
+    color: 0xeccda8,
+    texture: 'person.png',
+  },
+  {
+    querySelector: '.screamer',
+    form: 'square',
+    color: 0x4ca15f,
+    texture: 'screamer.png',
+  },
 ]
 
-for (let pin of allPins){
-    pins(pin.querySelector, pin.form, pin.color, pin.texture)
+for (let pin of allPins) {
+  pins(pin.querySelector, pin.form, pin.color, pin.texture)
 }
 
-function pins(querySelector, form = 'square', color = 0x000000, texture){
-    // Sizes
-    const sizes = {
-        width: 300,
-        height: 300
+function pins(querySelector, form = 'square', color = 0x000000, texture) {
+  // Sizes
+  const sizes = {
+    width: 300,
+    height: 300,
+  }
+
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color(0xffffff)
+
+  const loader = new GLTFLoader()
+
+  loader.load(
+    '/models/pin/scene.gltf',
+    function (gltf) {
+      scene.add(gltf.scene)
+    },
+    undefined,
+    function (error) {
+      console.error(error)
     }
+  )
 
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xffffff );
+  // Pins
+  const textureLoader = new THREE.TextureLoader()
+  const materials = [
+    new THREE.MeshLambertMaterial({
+      color: color,
+    }),
+    new THREE.MeshLambertMaterial({
+      color: color,
+    }),
+    new THREE.MeshLambertMaterial({
+      color: color,
+    }),
+    new THREE.MeshLambertMaterial({
+      color: color,
+    }),
+    new THREE.MeshLambertMaterial({
+      map: textureLoader.load(`src/page/pins/textures/${texture}`),
+    }),
+    new THREE.MeshLambertMaterial({
+      color: color,
+    }),
+  ]
+  let object
+  if (form == 'square') {
+    object = new THREE.Mesh(new THREE.BoxGeometry(28, 28, 1), materials)
+  } else {
+    object = new THREE.Mesh(new THREE.CircleGeometry(28, 28, 1), materials)
+  }
 
-    const loader = new GLTFLoader();
+  object.position.set(15, 19, 11)
+  object.rotation.set(-0.2, 0, 0)
+  scene.add(object)
 
-    loader.load('/models/pin/scene.gltf', function (gltf) {
-        scene.add(gltf.scene);
-    }, undefined, function ( error ) {
-        console.error( error );
-    })
+  // Light
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+  scene.add(ambientLight)
 
-    // Pins
-    const textureLoader = new THREE.TextureLoader();
-    const materials = [
-        new THREE.MeshLambertMaterial({
-            color: color
-        }),new THREE.MeshLambertMaterial({
-            color: color
-        }),new THREE.MeshLambertMaterial({
-            color: color
-        }),new THREE.MeshLambertMaterial({
-            color: color
-        }),new THREE.MeshLambertMaterial({
-            map: textureLoader.load(`src/page/pins/textures/${texture}`)
-        }),new THREE.MeshLambertMaterial({
-            color: color
-        })
-    ];
-    let object;
-    if(form == 'square'){
-        object = new THREE.Mesh(
-            new THREE.BoxGeometry( 28, 28, 1 ), materials
-        )
-    }else{
-        object = new THREE.Mesh(
-            new THREE.CircleGeometry( 28, 28, 1 ), materials
-        )
-    }
+  const spotLight = new THREE.PointLight(0xffffff, 2, 200, 0.01)
+  spotLight.position.set(0, 0, 0)
+  scene.add(spotLight)
 
-    object.position.set(15, 19, 11);
-    object.rotation.set(-0.2, 0, 0);
-    scene.add( object );
+  // Camera
+  const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height)
+  camera.position.x = 0
+  camera.position.z = 100
+  scene.add(camera)
 
-    // Light
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-    scene.add( ambientLight );
+  // Renderer
+  const canvas = document.querySelector(querySelector)
 
-    const spotLight = new THREE.PointLight(0xffffff, 2, 200, 0.01);
-    spotLight.position.set(0, 0, 0);
-    scene.add(spotLight);
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+  })
 
-    // Camera
-    const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
-    camera.position.x = 0
-    camera.position.z = 100;
-    scene.add(camera)
+  renderer.setSize(sizes.width, sizes.height)
 
+  // Controls
+  const controls = new OrbitControls(camera, canvas)
+  controls.enableDamping = true
 
+  // Animate
+  const animate = () => {
+    controls.update()
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(animate)
+  }
 
-    // Renderer
-    const canvas = document.querySelector(querySelector);
-
-    const renderer = new THREE.WebGLRenderer({
-        canvas: canvas
-    })
-
-    renderer.setSize(sizes.width, sizes.height)
-
-    // Controls
-    const controls = new OrbitControls( camera, canvas )
-    controls.enableDamping = true
-
-    // Animate
-    const animate = () => {
-        controls.update()
-        renderer.render( scene, camera );
-        window.requestAnimationFrame(animate)
-    }
-
-    animate()
+  animate()
 }
-
--- src/page/pins/sources/model.js
+++ src/page/pins/sources/model.js
@@ -1,62 +1,67 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 // MODEL DE PINS
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
+  width: 800,
+  height: 600,
 }
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xffffff );
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xffffff)
 
-const loader = new GLTFLoader();
+const loader = new GLTFLoader()
 
 // Model
-loader.load('/models/pin/scene.gltf', function (gltf) {
-    scene.add(gltf.scene);
-}, undefined, function ( error ) {
-    console.error( error );
-} )
+loader.load(
+  '/models/pin/scene.gltf',
+  function (gltf) {
+    scene.add(gltf.scene)
+  },
+  undefined,
+  function (error) {
+    console.error(error)
+  }
+)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height)
 camera.position.x = 0
-camera.position.z = 100;
-scene.add(camera);
+camera.position.z = 100
+scene.add(camera)
 
 // spotLight
-const spotLight = new THREE.PointLight(0xffffff, 5, 200, 0.01);
-spotLight.position.set(50, 50, 50);
-scene.add(spotLight);
+const spotLight = new THREE.PointLight(0xffffff, 5, 200, 0.01)
+spotLight.position.set(50, 50, 50)
+scene.add(spotLight)
 
-const spotLight2 = new THREE.PointLight(0xffffff, 5, 200, 0.01);
-spotLight2.position.set(0, 0, 0);
-scene.add(spotLight2);
+const spotLight2 = new THREE.PointLight(0xffffff, 5, 200, 0.01)
+spotLight2.position.set(0, 0, 0)
+scene.add(spotLight2)
 
 // Renderer
-const canvas = document.querySelector('.webgl');
+const canvas = document.querySelector('.webgl')
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 })
 
 renderer.setSize(sizes.width, sizes.height)
 
 // Controls
-const controls = new OrbitControls( camera, canvas )
+const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 // Animate
 const animate = () => {
-    controls.update()
+  controls.update()
 
-    renderer.render( scene, camera );
+  renderer.render(scene, camera)
 
-    window.requestAnimationFrame(animate)
+  window.requestAnimationFrame(animate)
 }
 
 animate()
