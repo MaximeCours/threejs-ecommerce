577b6975bc456a33a3dc2fed65bbbdeb8698510d
Maxime Perraud <contact@maxime-p.dev>
2023-06-21 09:34:55 +0200

    feat: trying cursor animation

--- src/page/pins/index.js
+++ src/page/pins/index.js
@@ -13,20 +13,31 @@ const scene = new THREE.Scene();
 
 const loader = new GLTFLoader();
 
-loader.load('/models/pin/scene.gltf', function (gltf) {
-  console.log('lama')
-  scene.add(gltf.scene);
-}, undefined, function ( error ) {
-
-  console.error( error );
-
-} )
+loader.load(
+  '/models/pin/scene.gltf',
+  function (gltf) {
+    const box = new THREE.Box3().setFromObject(gltf.scene)
+    const center = new THREE.Vector3()
+    box.getCenter(center)
+    gltf.scene.position.sub(center) // center the model
+    gltf.object.name = 'pins';
+    scene.add(gltf.scene)
+    const pinsObject = scene.getObjectByName('pins');
+  },
+  undefined,
+  function (error) {
+    console.error(error)
+  }
+)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
-camera.position.x = 0
-camera.position.z = 100;
-scene.add(camera)
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 100
+cameraGroup.add(camera)
 
 // Ambiant Light
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
@@ -50,13 +61,35 @@ renderer.setSize(sizes.width, sizes.height)
 const controls = new OrbitControls( camera, canvas )
 controls.enableDamping = true
 
+const cursor = {x: 0, y: 0}
+const clock = new THREE.Clock()
+let previousTime = 0
+
+window.addEventListener('mousemove', (event) =>
+{
+  cursor.x = event.clientX / sizes.width - 0.5
+  cursor.y = event.clientY / sizes.height - 0.5
+})
+
 // Animate
-const animate = () => {
-  controls.update()
+const tick = () =>
+{
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
 
-  renderer.render( scene, camera );
 
-  window.requestAnimationFrame(animate)
+  const parallaxX = cursor.x * 10
+  const parallaxY = - cursor.y * 10
+
+  pinsObject.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+  pinsObject.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
-animate()
+tick()
