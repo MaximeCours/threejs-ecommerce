16a61fec3664b383c7413f1016694696a722de9d
Maxime Perraud <contact@maxime-p.dev>
2023-06-21 10:14:48 +0200

    feat: header active item

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,12 +1,27 @@
 import './style.scss'
 
+const { pathname } = window.location
+
+const pages = [
+  { name: 'Home', url: '/' },
+  { name: 'Shoes', url: 'crocs.html' },
+  { name: 'Pins', url: 'pins.html' },
+  { name: 'Personalize it !', url: 'personnaliser.html' },
+]
+
+const path = pathname.length > 1 ? pathname.slice(1) : pathname // Get page name from URL
 document.querySelector('.header').innerHTML = `
 <header>
   <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="crocs.html">Shoes</a></li>
-    <li><a href="pins.html">Pins</a></li>
-    <li><a href="personnaliser.html">Personalize it !</a></li>
+    ${pages
+      .map(
+        (page) => `
+    <li ${path === page.url ? "class='active'" : ''}><a href="${page.url}">${
+          page.name
+        }</a></li>
+  `
+      )
+      .join('')}
   </ul>
 </header>
 `
